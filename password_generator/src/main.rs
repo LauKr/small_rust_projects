d4443a01102da9cb075522144acc0ae617c53864
Laurenz Kruty <gitlab@laurenzkruty.de>
2022-02-14 23:25:22 +0100

    Added more possible characters for password.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   password_generator/src/main.rs

--- password_generator/src/main.rs
+++ password_generator/src/main.rs
@@ -10,14 +10,18 @@ fn main() {
 
 
 fn generate_char_set() -> Vec<&'static str>{
-    let chars: Vec<&str> = vec!["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+    //let chars: Vec<&str> = vec!["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+    let chars: Vec<&str> = vec!["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", 
+    "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+    "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+    "!", "?", "§", "$", "%", "&", "/", "#", "+", "*", "~", "<", ">", ".", ":", ",", ";", "-", "_", "=", "(", ")", "[", "]", "{", "}"];
     return chars
 }
 
 
 fn generate_char<'a>(char_set: &std::vec::Vec<&'a str>) -> &'a str{
     let mut rng = rand::thread_rng();
-    let random_int: usize = rng.gen_range(0..10);
+    let random_int: usize = rng.gen_range(0..char_set.len());
     let rnd_char = char_set[random_int];
     //println!("Random number is {}", random_int);
     //println!("Random character from char_set is {}", rnd_char);
@@ -42,12 +46,7 @@ fn initialize() -> u64{
         .read_line(&mut passwd_length_string)
         .expect("Failed to read line");
     let passwd_length = passwd_length_string.trim().parse::<i32>().expect("invalid input");
-    /*let passwd_length = passwd_length_string.trim();
-    match passwd_length.parse::<u64>() {
-        Ok(i) => println!("your integer input: {}", i),
-        Err(..) => println!("this was not an integer: {}", passwd_length),
-    };*/
-    
+
     println!("Selected length is {}", passwd_length);
     return passwd_length.try_into().unwrap()
 }
